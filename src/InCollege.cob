a0cb411bbaf4f6d16c7e59923df6ecf0286245a5
nkh5820 <nkholland30@gmail.com>
2025-09-07 06:19:52 -0400

    feat: create module for account creation and integrate into main program

--- src/InCollege.cob
+++ src/InCollege.cob
@@ -11,18 +11,33 @@ FILE-CONTROL.
 DATA DIVISION.
 FILE SECTION.
 FD  INFILE.
-01  IN-REC     PIC X(100).
+01  IN-REC             PIC X(100).
 
 WORKING-STORAGE SECTION.
-01  EOF        PIC X VALUE "N".
+01  EOF                PIC X(1) VALUE "N".
+01  CURRENT-LINE       PIC X(100).
+01  PARSED-INPUT.
+   05  COMMAND     PIC X(10).
+   05  ARGS        PIC X(100).
 
 PROCEDURE DIVISION.
     OPEN INPUT INFILE
+
     PERFORM UNTIL EOF = "Y"
         READ INFILE
-            AT END MOVE "Y" TO EOF
-            NOT AT END DISPLAY IN-REC
+            AT END
+                MOVE "Y" TO EOF
+            NOT AT END
+                UNSTRING IN-REC DELIMITED BY ","
+                    INTO COMMAND, ARGS
+                END-UNSTRING
+
+                EVALUATE COMMAND
+                    WHEN "CREATE"
+                        CALL 'CREATE-ACCOUNT' USING ARGS
+                END-EVALUATE
         END-READ
     END-PERFORM
-    CLOSE INFILE
+
+    CLOSE INFILE.
     STOP RUN.
